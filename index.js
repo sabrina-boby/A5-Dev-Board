4ddaaaff84f5644d22576bc34b679eccec33b07c
sabrina-boby <sabrinaboby786@gmail.com>
2025-03-01 02:17:21 +0600

    code update

--- index.js
+++ index.js
@@ -17,3 +17,50 @@ function updateDate() {
   dateElement.textContent = formattedDate;
 }
 updateDate();
+
+// discover-new-page
+document.getElementById("discover-new").addEventListener("click", function () {
+  window.location.href = "discover-something.html";
+});
+
+//add card
+let taskAssigned = document.getElementById("task-assigned");
+let taskAssignedData = parseInt(taskAssigned.innerText);
+
+let addedCardCount = document.getElementById("added-card-count");
+let addedCardCountAll = parseInt(addedCardCount.innerText);
+
+let addedActivity = document.getElementById("added-activity");
+
+let buttons = document.getElementsByClassName("completed-btn");
+
+let currentTime = new Date().toLocaleTimeString();
+
+for (let btn of buttons) {
+  btn.addEventListener("click", function () {
+    // button disabled
+    alert("Board updated Successfully");
+    this.classList.add("disabled");
+    // increse number
+    taskAssignedData -= 1;
+    taskAssigned.innerText = taskAssignedData;
+    if (taskAssignedData <= 0) {
+      alert("congrates!!!You have completed all the current task");
+    }
+    // decrese number
+    addedCardCountAll += 1;
+    addedCardCount.innerText = addedCardCountAll;
+    let cardTitle =
+      this.closest(".hero").querySelector(".card-title").innerText;
+    addedActivity.innerHTML += `
+      <p class="bg-[#e0e2e9] p-2 rounded">You have Complete The Task ${cardTitle} at ${currentTime}</p>
+  `;
+  });
+
+  // clearHistory
+  let clearHistory = document
+    .getElementById("clear-history")
+    .addEventListener("click", function () {
+      addedActivity.innerHTML = ``;
+    });
+}
